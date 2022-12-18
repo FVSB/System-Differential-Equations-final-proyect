b80f606589e3cfe2de6d7d65ab02eee0efdf6a1b
Francisco Vicente Suarez Bellon <95106875+FVSB@users.noreply.github.com>
2022-12-18 02:16:25 -0500

    Add (0,0)
    Signed-off-by: Francisco Vicente Suarez Bellon <95106875+FVSB@users.noreply.github.com>

--- auxilar.py
+++ auxilar.py
@@ -1,21 +1,22 @@
 from py_expression_eval import Parser
 
+
 class funt:
-   def __init__(self,expression):
-    self.exp=expression
-    parser=Parser()
-    v=parser.parse(expression)
-    self.vars=v.variables()
-    self.new_express=None
-    
-   def evaluate_params(self,params:dict):
-     parser=Parser()
-     x=parser.parse(self.exp)
-     self.new_express=x.simplify(params).toString()
-    
-   def evaluate(self,x,y,t):
-     parser=Parser()
-     return parser.evaluate(self.new_express,{'x':x,'y':y,'t':t})
-    
-   def string(self):
-     return self.new_express
+    def __init__(self, expression):
+        self.exp = expression
+        parser = Parser()
+        v = parser.parse(expression)
+        self.vars = v.variables()
+        self.new_express = None
+
+    def evaluate_params(self, params: dict):
+        parser = Parser()
+        x = parser.parse(self.exp)
+        self.new_express = x.simplify(params).toString()
+
+    def evaluate(self, x, y, t):
+        parser = Parser()
+        return parser.evaluate(self.new_express, {'x': x, 'y': y, 't': t})
+
+    def string(self):
+        return self.new_express
